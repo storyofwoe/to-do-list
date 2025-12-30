acfc7ebd64529790be9d25d91ed9b9fe5a673f21
juliet <53350096+storyofwoe@users.noreply.github.com>
2025-12-30 20:57:50 +0000

    Create and development of main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+#py -m pip install <package> in cmd terminal
+import inflect
+p = inflect.engine()
+
+reminders = ["test", "test2"]
+remindLen = len(reminders)
+
+while True:
+    print(f"There {p.plural("is", remindLen)} currently {remindLen} {p.plural("reminder",remindLen)} in your list.")
+
+    ext = ""
+    if remindLen > 0:
+        ext = ", or 'EDIT' followed by its ID to edit a reminder"
+    print("Type 'CREATE' to create one" + ext + ".")
+    print()
+
+    if remindLen > 0:
+        print("ID  |  Reminder")
+        print("----+----------")
+        for x in range(remindLen):
+            print(f"{str(x+1).ljust(3)} | {reminders[x]}") #consider using textwrap package to wrap text if it's too long
+
+    print()
+
+    cmd = input().lower()
+    if cmd == "create":
+        print("Success!")
+    elif cmd == "edit":
+        pass
